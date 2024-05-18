c62a43c8dbd4e06ba0439074f48bb3c509eca80c
Ravi Raushan <raviraushanrai@gmail.com>
2024-05-18 08:37:42 +0530

    fix: start script added

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const express = require("express");
+import express from "express";
 
 const app = express();
-
 const PORT = 5000;
-app.listen(GET, () => {
+
+app.all("/", (req, res) => {
+  console.log(`Request > ${req}`);
+  console.log(`Response > ${res}`);
+
+  res.send("I am up!!");
+});
+
+app.listen(PORT, () => {
   console.log(`Server is running at port: ${PORT}`);
 });
