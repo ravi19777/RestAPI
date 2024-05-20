d17d1a8cf277a321bab0f7a9beb167dd66ff2b41
Ravi Raushan <raviraushanrai@gmail.com>
2024-05-20 19:32:58 +0530

    fix: restro data sent in response

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import restaurants from "./data.js";
 
 const app = express();
 const PORT = 5000;
@@ -7,7 +8,7 @@ app.get("/", (req, res) => {
   // Anything which is coming from client, will be the part of the 'req'
   // Anything which is coming from the server, will be the part of the 'res'
 
-  res.send("I am up!!");
+  res.send(restaurants);
 });
 
 app.listen(PORT, () => {
