f036058b07f9c84ac3aa0a1c16caba556d7e5bf5
Ravi Raushan <raviraushanrai@gmail.com>
2024-05-21 07:18:10 +0530

    fix: Body parser added to the express code

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 import express from "express";
 import restaurants from "./data.js";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = 5000;
+
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   // Anything which is coming from client, will be the part of the 'req'
   // Anything which is coming from the server, will be the part of the 'res'
 
   res.send(restaurants);
+});
+
+app.post("/register", (req, res) => {
+  res.send("Please enter the details of restaurants to register it!!");
 });
 
 app.listen(PORT, () => {
